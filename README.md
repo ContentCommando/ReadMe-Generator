f250519aaaed8c2de853c602f137a7e31214fa19
Joseph Ononaye <josephononaye@gmail.com>
2023-11-01 00:13:09 +0000

    Added dependencies for prompts named Inquirer and developed code to write a README.md file from the command line

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# ReadMe-Generator
-An application for generation project README.md files using the command line interface.
+
+#### Table of contents
+[Features](#description)
+
+[Languages](#stack)
+
+[New Tools](#experience)
+
+[Future Prospects](#future-prospects)
+
+[License](#license)
+
+# README Generator
+This is a README generator that allows developers to create documentations for their projects quickly from the Command Line Interface. [See Link](https://watch.screencastify.com/v/gQvVMitoBtVfzJrGaUSF) to demo video.
+
+### Features
+The application is written in Node.js and launches from the command line. It features prompts that elicit essential data that should be contained in a concise README.md document. It's a time-saving application for developers.
+### Languages 
+ JavaScript, Node.js.
+### New Tools     
+The Inquirer npm was deployed in developing this application. It was a quick and easy way to integrate prompts into the application without having to create them from the start. Using the Node runtime is a time-saving and less-complicated mode of building applications as I do not need to run codes through the browser/DOM.
+### Future Prospects    
+To add more specific and wide-ranging prompts so that the application can be customisable to different types of projects and developers. And to also make badges and licenses displayable on GitHub
+### License  
+ ISC
--- index.js
+++ index.js
@@ -1,2 +1,73 @@
-const power = require("inquirer");
+const inquirer = require("inquirer");
 const fs = require("fs");
+
+const questions = [
+    {
+      type: 'input',
+      name: 'title',
+      message: "What's the title of your project?"
+    },
+   
+    {
+      type: 'input',
+      name: 'description',
+      message: "Write a brief description of your project"
+    },
+
+    {
+      type: 'input',
+      name: 'features',
+      message: "List and describe the main features of your project"
+    },
+
+    {
+      type: 'checkbox',
+      message: 'Which programming languages were used?',
+      name: 'stack',
+      choices: [' HTML', ' CSS', ' JavaScript', ' MySQL', ' jQuery', ' C++', " Node.js"]
+    },
+
+    {
+      type: 'input',
+      message: 'Describe your experience with new tools you used, e.g, dependencies, runtimes, etc',
+      name: 'experience'
+    },
+
+    {
+      type: 'input',
+      name: 'future',
+      message: "What are the future prospects of the application?"
+    },
+    
+    {
+      type: 'checkbox',
+      message: 'Which license do you prefer for your project?',
+      name: 'license',
+      choices: [' AFL-3.0', ' Apache-2.0', ' Artistic-2.0', ' BSL-1.0', ' MIT', ' ISC', " LGPL-2.1"]
+    },
+]
+  inquirer.prompt(questions).then((answers) => {
+    const formattedAnswer =`
+#### Table of contents
+[Features](#description)\n
+[Languages](#stack)\n
+[New Tools](#experience)\n
+[Future Prospects](#future-prospects)\n
+[License](#license)\n
+# ${answers.title}
+${answers.description}
+### Features
+${answers.features}
+### Languages 
+${(answers.stack)}.
+### New Tools     
+${answers.experience}
+### Future Prospects    
+${answers.future}
+### License  
+${answers.license}
+`
+    fs.writeFile("READMEgen.md", formattedAnswer, (err) => {
+        err ? console.log(err) : console.log("Successful");
+    })
+  });
